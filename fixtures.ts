cf437f511955899f55812de985cf88f0706accd6
Saradha Mohan <saradha@quarksek.com>
2026-09-24 16:08:16 +0530

    commit change in Fixture files

---
+++ fixtures.ts
@@ -0,0 +1,37 @@
+import { test as base } from '@playwright/test';
+import { LoginPage } from './pages/LoginPage';
+import { ProductPage } from './pages/ProductPage';
+import { CartPage } from './pages/CartPage';
+//import { SupportPage } from './pages/SupportPage';
+
+type MyFixtures = {
+    loginPage: LoginPage;
+    productPage: ProductPage;
+    cartPage: CartPage;
+    //supportPage: SupportPage;
+};
+
+export const test = base.extend<MyFixtures>({
+
+    loginPage: async ({ page }, use) => {
+        const loginPage = new LoginPage(page);
+        await use(loginPage);
+    },
+
+    productPage: async ({ page }, use) => {
+        const productPage = new ProductPage(page);
+        await productPage.navigateTo();
+        await use(productPage);
+    },
+
+    cartPage: async ({ page }, use) => {
+        const cartPage = new CartPage(page);
+        //await cartPage.navigateTo();
+        await use(cartPage);
+    },
+    // supportPage: async ({ page }, use) => {
+    //     await use(new SupportPage(page));
+    // },
+});
+
+export { expect } from '@playwright/test';
