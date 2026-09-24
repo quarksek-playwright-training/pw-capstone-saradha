6d209470177edd8fbbf1f0a9306bbdb2c6d35ad8
Saradha Mohan <saradha@quarksek.com>
2026-09-24 16:17:19 +0530

    committed the abnormal failing test

---
+++ tests/abnormal-state.spec.ts
@@ -0,0 +1,40 @@
+import { test, expect } from "../fixtures";
+
+test(
+    "Application handles Add to Cart request failure",
+    {
+        tag: ["@regression", "@cart", "@negative"],
+    },
+    async ({ page, productPage, cartPage }) => {
+
+        const productName = "Men Tshirt";
+
+        // Make sure this product is not already in the cart
+        await cartPage.navigateTo();
+
+        if (await cartPage.isProductInCart(productName)) {
+            await cartPage.removeProduct(productName);
+            await cartPage.navigateTo();
+        }
+
+        await page.route(
+            "**/add_to_cart/**",
+            async (route) => {
+                await route.abort("failed");
+            }
+        );
+
+        await productPage.navigateTo();
+        await productPage.searchProduct(productName);
+
+        await productPage.addProductToCart(productName);
+
+        await cartPage.navigateTo();
+
+        await expect(
+            page
+                .locator("#cart_info_table tbody tr")
+                .filter({ hasText: productName })
+        ).toHaveCount(0);
+    }
+);
